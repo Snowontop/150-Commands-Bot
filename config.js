ccd038b6a5a1c2adf4a763fcfaefd9bf1641b25b
Snowontop <86823634+Snowontop@users.noreply.github.com>
2021-07-28 08:56:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 exports.TOKEN = '';
 
-exports.PREFIX = '';
+exports.PREFIX = 'S!';
 
-exports.news_API = '';
+exports.news_API = 'e96bf77d08b5429285b9ef13f5e2793c';
 
-exports.giphy_API = '';
+exports.giphy_API = 'oCZWKCRNh4iiXPRbdcssvFqhRpAw5Yfa';
 
-exports.AME_API = '';
+exports.AME_API = 'bb03f373caa534fcfcbaeae177a65134f44a6e57ba7a7b098be273867b376d8a677ddae3c23c6ded4fec8288573945e8c3483689deb13f229376ad4b5b60231d';
